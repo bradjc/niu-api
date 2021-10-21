816b54eae87a39a18927d148433212bca01e0054
Gino Naumann <gino@nauminator.de>
2021-10-21 23:51:52 +0200

    chore: python setup

---
+++ niuApi/__init__.py
@@ -0,0 +1,10 @@
+"""Wrapper for NIU API
+"""
+
+NAME = 'NIU API'
+VERSION = '0.0'
+DESCRIPTION = __doc__
+
+__author__ = 'Gino Naumann'
+__license__ = 'MIT'
+__version__ = VERSION
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from niuApi import (__author__, __license__, NAME, VERSION, DESCRIPTION)
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION.split('\n')[0],
+    author=__author__,
+    author_email='gino@nauminator.de',
+    url='https://github.com/genaumann/niu-api',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    entry_points={
+        'console_scripts': [
+            'niuapi = niuApi.cli:run',
+        ]
+    },
+    include_package_data=True,
+    install_requires=['pyyaml', 'argparse'],
+    license=__license__,
+    zip_safe=False,
+)

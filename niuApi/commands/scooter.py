bd7806a0a0cb7463e714144a93c1b869fc10d599
Gino Naumann <gino@nauminator.de>
2021-11-08 01:50:50 +0100

    docs(scooter.list): adjust docs

--- niuApi/commands/scooter.py
+++ niuApi/commands/scooter.py
@@ -3,8 +3,11 @@ import niuApi.apicommands as apicommands
 def list(print: list = ['sn_id'], **kwargs) -> dict:
     """Get all scooters conntected with the desired accout
 
+    Args:
+        print (list, optional): Set the options to print. Defaults to ['sn_id'].
+
     Returns:
-        list: all scooters
+        dict: key: serial number, values: values in print option
     """
 
     scooters = apicommands.v5.scooter_list()

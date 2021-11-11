137db8cb039fab19edd2793a9f15f8785426f024
Gino Naumann <gino@nauminator.de>
2021-11-12 00:16:32 +0100

    feat(battery.info): add new function

--- niuApi/commands/battery.py
+++ niuApi/commands/battery.py
@@ -46,3 +46,57 @@ def list(serial: str = None, print: list = ['bmsId'], **kwargs) -> dict:
     out[sn] = battery_info
 
     return out
+
+def info(serial: str = None, bmsid: str = None, print: list = ['bmsId', 'batteryCharging'], **kwargs) -> dict:
+    """Get info of the batteries conntected to the scooter
+
+    Args:
+        serial (str, optional): Serial number of given scooter. Defaults to None.
+        bmsid (str, optional): Battery id. Defaults to None.
+        print (list, optional): Print following options. Defaults to ['bmsId', 'batteryCharging'].
+
+    Returns:
+        dict: key: serial number, values: batteries with subkeys and subvalues
+    """
+
+    possible_prints = [
+        'bmsId', 'batteryCharging', 'chargedTimes',
+        'energyConsumedToday', 'gradeBattery', 'isConnected',
+        'temperature', 'temperatureDesc'
+    ]
+
+    scooters = apicommands.v5.scooter_list()
+
+    out = {}
+    battery_info = {}
+    for scooter in scooters:
+        sn = scooter.get('sn_id')
+        
+        if serial is not None:
+            if sn != serial:
+                continue
+        
+        batteries = apicommands.v3.motor_data_battery_info(sn).get('batteries')
+        i = 1
+        for values in batteries.values():
+
+            id = values.get('bmsId')
+            if not id:
+                battery_info[i] = {}
+                battery_info[i]['info'] = f'Battery {i}: not connected'
+                i += 1
+                continue
+            
+            if bmsid is not None and bmsid != id: continue
+
+            battery_info[id] = {}
+
+            for arg in print:
+                try:
+                    if arg in possible_prints: battery_info[id][arg] = values[arg]
+                except KeyError:
+                    pass
+            
+    out[sn] = battery_info
+
+    return out

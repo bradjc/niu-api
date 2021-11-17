4fd4612861b64251e72c0db6951691055e96f2dd
Gino Naumann <gino@nauminator.de>
2021-11-17 01:31:15 +0100

    feat(trips.get): add new command

---
+++ niuApi/commands/trips.py
@@ -0,0 +1,66 @@
+from datetime import date
+
+import niuApi.apicommands as apicommands
+
+today = date.today()
+
+def get(
+    serial: str = None,
+    print: list = ['everdayMileage'],
+    date: list = [today.strftime("%Y%m%d")],
+) -> dict:
+    """Get trip information by date (max. 29 days)
+
+    serial
+        Serial number of scooter to limit output.
+    
+    print
+        Limit output for keys:
+            [AvgSpeed, date, everdayMileage,
+            maxSpeed, 'ridingTime]
+        Default = [everdayMileage]
+
+    CLI Examples:
+        niu-api trips.get serial=S3R1ALOFSC00T3R print=maxSpeed,everdayMileage
+        niu-api trips.get print=everdayMileage date=20211117
+        niu-api trips.get print=AvgSpeed,maxSpeed date=20211117,20211116,20211115
+    """
+
+    possible_prints = [
+        'AvgSpeed', 'date', 'everdayMileage',
+        'maxSpeed', 'ridingTime'
+    ]
+
+    if not isinstance(date, list): date = [ date ] # TODO
+
+    scooters = apicommands.v5.scooter_list()
+
+    out = {}
+    trip_info = {}
+    for scooter in scooters:
+        sn = scooter.get('sn_id')
+
+        if serial is not None:
+            if sn != serial:
+                continue
+
+        trips = apicommands.v3.motor_data_cycling_statistics(sn).get('items')
+        for trip in trips:
+
+            if trip.get('date') not in date: continue
+
+            trip_info[trip.get('date')] = {}
+            for arg in print:
+                try:
+                    if arg in possible_prints: trip_info[trip.get('date')][arg] = trip[arg]
+                except KeyError:
+                    pass
+
+        # sort dict
+        index_map = {v: i for i, v in enumerate(date)}
+        out[sn] = {}
+        for index in sorted(trip_info.items(), key=lambda pair: index_map[pair[0]]):
+            for key in index:
+                out[sn][index[0]] = key
+
+    return out

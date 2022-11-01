f07012e2c64eb9854fbd417b1fb9a2f2ab186bc0
Brad Campbell <bradjc5@gmail.com>
2022-10-31 21:26:26 -0400

    change track list to what worked in Oct 2022

--- niuApi/apicommands/v5.py
+++ niuApi/apicommands/v5.py
@@ -1,88 +1,88 @@
 from niuApi.requests import do_request
 from niuApi.exceptions import NIURequestError
 
+
 def scooter_list():
 
-    json_response = do_request('v5/scooter/list')
+    json_response = do_request("v5/scooter/list")
 
-    if len(json_response.get('data').get('items')) == 0:
-        raise NIURequestError('No scooter found')
+    if len(json_response.get("data").get("items")) == 0:
+        raise NIURequestError("No scooter found")
 
     scooters = []
-    for scooter in json_response.get('data').get('items'):
+    for scooter in json_response.get("data").get("items"):
         scooters.append(scooter)
-    
+
     return scooters
+
 
 def scooter_detail(serial):
 
     json_response = do_request(
-        f'v5/scooter/detail/{serial}',
+        f"v5/scooter/detail/{serial}",
         add_headers={
-            'user-agent': 'manager/4.6.44 (nuiAPI);lang=en-US;clientIdentifier=Overseas'
-        }
-    ).get('data')
+            "user-agent": "manager/4.6.44 (nuiAPI);lang=en-US;clientIdentifier=Overseas"
+        },
+    ).get("data")
 
     if len(json_response.keys()) == 0:
-        raise NIURequestError('No scooter details returned')
-    
+        raise NIURequestError("No scooter details returned")
+
     return json_response
+
 
 def scooter_motor_data_index_info(serial):
 
     json_response = do_request(
-        'v5/scooter/motor_data/index_info',
+        "v5/scooter/motor_data/index_info",
         add_headers={
-            'user-agent': 'manager/4.6.44 (nuiAPI);lang=en-US;clientIdentifier=Overseas'
+            "user-agent": "manager/4.6.44 (nuiAPI);lang=en-US;clientIdentifier=Overseas"
         },
-        add_params={'sn': serial}
-    ).get('data')
+        add_params={"sn": serial},
+    ).get("data")
 
     if len(json_response.keys()) == 0:
-        raise NIURequestError('No motor_data returned')
+        raise NIURequestError("No motor_data returned")
 
     return json_response
+
 
 def tire_gauge_realtime(serial):
 
-    json_response = do_request(
-        'v5/tire_gauge/realtime',
-        add_params={'sn': serial}
-    ).get('data')
+    json_response = do_request("v5/tire_gauge/realtime", add_params={"sn": serial}).get(
+        "data"
+    )
 
     if len(json_response.keys()) == 0:
-        raise NIURequestError('No tire data returned')
+        raise NIURequestError("No tire data returned")
 
     return json_response
+
 
 def tire_gauge_status(serial):
 
-    json_response = do_request(
-        'v5/tire_gauge/status',
-        add_params={'sn': serial}
-    ).get('data')
+    json_response = do_request("v5/tire_gauge/status", add_params={"sn": serial}).get(
+        "data"
+    )
 
     if len(json_response.keys()) == 0:
-        raise NIURequestError('No tire data returned')
+        raise NIURequestError("No tire data returned")
 
     return json_response
+
 
 def track_list_v2(serial, pagesize=10, index=0):
 
     json_response = do_request(
-        'v5/track/list/v2',
-        method='post',
-        add_data={
-            'index': str(index),
-            'pagesize': pagesize,
-            'sn': serial
-        },
+        "v5/k/track_list",
+        method="get",
+        add_params={"index": str(index), "pageSize": pagesize, "sn": serial},
         add_headers={
-            'user-agent': 'manager/4.6.44 (nuiAPI);lang=en-US;clientIdentifier=Overseas'
-        }
-    ).get('data')
+            "user-agent": "manager/4.6.44 (nuiAPI);lang=en-US;clientIdentifier=Overseas"
+        },
+    ).get("data")
 
     if len(json_response.keys()) == 0:
-        raise NIURequestError('No track data returned')
+        raise NIURequestError("No track data returned")
 
     return json_response
